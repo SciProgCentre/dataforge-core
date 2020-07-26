34110f6be15c55c4b0357f86fc25aa10ad0512e5
Alexander Nozik <altavir@gmail.com>
2020-07-26 17:35:44 +0300

    Remove function clients and servers

--- dataforge-io/src/commonMain/kotlin/hep/dataforge/io/Responder.kt
+++ dataforge-io/src/commonMain/kotlin/hep/dataforge/io/Responder.kt
@@ -1,8 +1,21 @@
 package hep.dataforge.io
 
+import hep.dataforge.meta.DFExperimental
+
+/**
+ * An object that could respond to external messages asynchronously
+ */
 interface Responder {
     /**
      * Send a request and wait for response for this specific request
      */
     suspend fun respond(request: Envelope): Envelope
+}
+
+/**
+ * A fire-and-forget consumer of messages
+ */
+@DFExperimental
+interface Consumer {
+    fun consume(message: Envelope): Unit
 }
