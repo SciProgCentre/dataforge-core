25cba0c6de7ba0808c924c77f737def77eb98821
Alexander Nozik <altavir@gmail.com>
2021-01-26 18:26:28 +0300

    bump version to 0.3.0-dev-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("ru.mipt.npm.project")
 }
 
-val dataforgeVersion by extra("0.3.0-dev")
+val dataforgeVersion by extra("0.3.0-dev-1")
 
 val bintrayRepo by extra("dataforge")
 val githubProject by extra("dataforge-core")
