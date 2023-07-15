a136db16ff9fb0de938d124ac0fe9f3408ccc030
Alexander Nozik <altavir@gmail.com>
2023-07-15 10:49:42 +0300

    Refactoring to kotlinx-io complete

--- dataforge-workspace/build.gradle.kts
+++ dataforge-workspace/build.gradle.kts
@@ -10,17 +10,35 @@ kscience{
     useSerialization{
         protobuf()
     }
-    dependencies {
-        api(projects.dataforgeContext)
-        api(projects.dataforgeData)
-        api(projects.dataforgeIo)
-    }
-    dependencies(jvmTest){
-        implementation(spclibs.logback.classic)
-        implementation(projects.dataforgeIo.dataforgeIoYaml)
-    }
+//    dependencies {
+//        api(projects.dataforgeContext)
+//        api(projects.dataforgeData)
+//        api(projects.dataforgeIo)
+//    }
+//    dependencies(jvmTest){
+//        implementation(spclibs.logback.classic)
+//        implementation(projects.dataforgeIo.dataforgeIoYaml)
+//    }
 }
 
 readme{
     maturity = space.kscience.gradle.Maturity.EXPERIMENTAL
+}
+
+kotlin{
+    sourceSets{
+        commonMain{
+            dependencies {
+                api(projects.dataforgeContext)
+                api(projects.dataforgeData)
+                api(projects.dataforgeIo)
+            }
+        }
+        getByName("jvmTest"){
+            dependencies {
+                implementation(spclibs.logback.classic)
+                implementation(projects.dataforgeIo.dataforgeIoYaml)
+            }
+        }
+    }
 }
