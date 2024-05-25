3de5691c84dab040e7e570567b8b1fa38228b744
Alexander Nozik <altavir@gmail.com>
2024-05-25 21:19:06 +0300

    Add custom coroutine context during new Context creation

--- dataforge-context/src/commonMain/kotlin/space/kscience/dataforge/context/Context.kt
+++ dataforge-context/src/commonMain/kotlin/space/kscience/dataforge/context/Context.kt
@@ -10,6 +10,7 @@ import space.kscience.dataforge.misc.ThreadSafe
 import space.kscience.dataforge.names.Name
 import space.kscience.dataforge.provider.Provider
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 /**
  * The local environment for anything being done in DataForge framework. Contexts are organized into tree structure with [Global] at the top.
@@ -26,6 +27,7 @@ public open class Context internal constructor(
     public val parent: Context?,
     plugins: Set<Plugin>, // set of unattached plugins
     meta: Meta,
+    coroutineContext: CoroutineContext = EmptyCoroutineContext,
 ) : Named, MetaRepr, Provider, CoroutineScope {
 
     /**
@@ -65,7 +67,7 @@ public open class Context internal constructor(
 
     override val coroutineContext: CoroutineContext by lazy {
         (parent ?: Global).coroutineContext.let { parenContext ->
-            parenContext + SupervisorJob(parenContext[Job])
+            parenContext + coroutineContext + SupervisorJob(parenContext[Job])
         }
     }
 
