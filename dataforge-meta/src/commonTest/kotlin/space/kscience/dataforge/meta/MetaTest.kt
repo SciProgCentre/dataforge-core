b1cfb031871db90ca632c83704a3e09de43a5dd7
Колпаков Максим <mkolpakov2002@yandex.ru>
2025-10-03 03:01:42 +0300

    test(meta): Добавлены новые тесты для Value и ioMisc

--- dataforge-meta/src/commonTest/kotlin/space/kscience/dataforge/meta/MetaTest.kt
+++ dataforge-meta/src/commonTest/kotlin/space/kscience/dataforge/meta/MetaTest.kt
@@ -3,6 +3,7 @@ package space.kscience.dataforge.meta
 import space.kscience.dataforge.misc.DFExperimental
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class MetaTest {
     @Test
@@ -90,4 +91,39 @@ class MetaTest {
         println(oldMeta)
         assertEquals(setOf("a", "b", "d"), oldMeta.items.keys.map { it.toString() }.toSet())
     }
+
+    @Test
+    fun testValueParseQuotedString() {
+        assertEquals(StringValue("abc"), Value.parse("\"abc\""))
+        assertEquals(StringValue(""), Value.parse("\"\""))
+        assertEquals(StringValue("\""), Value.parse("\""))
+    }
+
+    @Test
+    fun `numeric equality implies equal hash codes`() {
+        val intOne: Value = Value.of(1)
+        val doubleOne: Value = Value.of(1.0)
+
+        assertEquals(intOne, doubleOne, "1 and 1.0 must be equal as Value")
+
+        assertEquals(
+            intOne.hashCode(),
+            doubleOne.hashCode(),
+            "Equal numeric Values must produce equal hash codes"
+        )
+
+        val set = hashSetOf(intOne)
+        assertTrue(doubleOne in set, "HashSet containment must work for equal numeric Values")
+    }
+
+    @Test
+    fun `minus zero and plus zero must hash equally when equal`() {
+        val a = Value.of(-0.0)
+        val b = Value.of(0.0)
+
+        assertEquals(a, b, "(-0.0) and (+0.0) must be equal as Value")
+
+        assertEquals(a.hashCode(), b.hashCode(), "Equal numeric Values must produce equal hash codes")
+    }
+
 }
